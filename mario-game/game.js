a80d8ee1b90d6f932f76a6b27322150be3ecf264
lmontxz <118456918+lmontxz@users.noreply.github.com>
2023-02-11 21:26:29 -0300

    Update game.js

--- mario-game/game.js
+++ mario-game/game.js
@@ -1,5 +1,7 @@
 const mario = document.querySelector('.mario')
 const cano = document.querySelector('.cano')
+const score = document.querySelector('.score')
+let count = 0;
 
 const pulo = () => {
     mario.classList.add('pulo');
@@ -22,8 +24,27 @@ const loop = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`; 
 
         mario.src = './imagens/mariomorto.png'
-        mario.style.style.marginLeft = '50px'
+        mario.style.marginRight = '100px'
+
+        clearInterval(loop);
     }
 }, 10);
+
+setInterval(() => {
+    let marioBottom = parseInt(
+        window.getComputedStyle(mario).getPropertyValue("bottom")
+    );
+    let canoLeft = parseInt(
+        window.getComputedStyle(cano).getPropertyValue("left")
+    );
+
+    if (canoLeft > 40 && canoLeft < 120 && marioBottom <= 50){
+        ('GameOver! Seu Score foi: ${count}');
+        count = 0;
+    }
+
+    count++;
+    score.innerHTML = `SCORE: ${count}`;
+}, 100);
 
 document.addEventListener('keydown', pulo)
